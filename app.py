16f9507840b5a83b1818b14b5c3863aae31713ef
gonlyplease <gerhi13@gmail.com>
2025-02-08 18:04:39 +0100

    Added first html preview

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
+import matplotlib
+
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import os
 
@@ -12,7 +15,28 @@ df = pd.read_csv(data_path)
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+
+
+@app.route("/get_plot", methods=["GET", "POST"])
+def get_plot():
+    if request.method == "POST":
+        plt.plot([1, 2, 3])
+        plt.title("Line Plot")
+        plt.show()
+        plt.savefig("static/line_plot.png")
+        return render_template("index.html", plot_url="static/line_plot.png")
+    else:
+        pass
+
+
+### Get the natural language input
+@app.route("/get_nl_input", methods=["GET", "POST"])
+def get_nl_input():
+    if request.method == "POST":
+        nl_input = request.form["nl_input"]
+        print(nl_input)
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
